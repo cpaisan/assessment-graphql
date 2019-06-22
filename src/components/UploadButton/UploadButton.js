eed4b7b77f981714fa5f6c2dfbd2be369bb95c41
Charlie Paisan <cppaisan@gmail.com>
2019-06-21 22:36:13 -0700

    [feature] Base UploadButton component

---
+++ src/components/UploadButton/UploadButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Material UI
+import Button from "@material-ui/core/Button";
+
+const UploadButton = props => {
+  return <Button>Upload</Button>;
+};
+
+export default UploadButton;
